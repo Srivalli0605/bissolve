4558a2d48f9273aa6f859b8acf66e8df1df1242e
Sri Varsha <srivarshapinninty@gmail.com>
2025-08-02 01:09:58 +0530

    added frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App"; // This is your home page
+import Signup from "./pages/Signup.tsx"; // Signup page
+import "./index.css";
+import Home from "./pages/Home"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-) 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+       
+
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
